feafdd2d084d5778e848e0358db09c40f6761656
tungnguyen741 <tung.nguyen21098@gmail.com>
2020-05-08 15:30:41 +0700

    ''

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cookieParser(process.env.sessionKey));
-
+const port = process.env.PORT || 3000; 
 // MONGO DB
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
@@ -61,7 +61,8 @@ app.get("/logout", async function (req, res) {
     await Session.deleteOne({sskey: req.signedCookies.sessionId});
     res.redirect("/login");
 });
-app.listen(3000, () => {
+
+app.listen(port, () => {
     console.log("OK!!!");
 });
 
