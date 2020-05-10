f737d3dc2d4c9b7d29d94a02771b59066769d8cd
tungnguyen741 <tung.nguyen21098@gmail.com>
2020-05-10 15:27:58 +0700

    ''

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -94,7 +94,7 @@ module.exports.postUpdateUser = async function (req, res, next) {
     if (req.file) {
         req.body.avatar = req.file.path.split("\\").slice(1).join('/');
         try{
-          var uploader = await cloudinary.v2.uploader.upload('./public/'+ req.body.avatar);
+          var uploader = await cloudinary.v2.uploader.upload(req.body.avatar);
           var hashPass = await bcrypt.hash(req.body.password, saltRounds);
           var updateUs = await User.updateOne({_id: id}, { 
                 name:  req.body.name,
